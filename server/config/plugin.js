27609d63e0a019ec4b3ca2331d6f3c3d310d2e9f
liuwenlong916 <1141657011@qq.com>
2021-04-09 10:47:55 +0800

    添加mysql

--- server/config/plugin.js
+++ server/config/plugin.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /** @type Egg.EggPlugin */
 module.exports = {
@@ -6,4 +6,9 @@ module.exports = {
   // static: {
   //   enable: true,
   // }
+};
+
+exports.mysql = {
+  enable: true,
+  package: "egg-mysql",
 };
