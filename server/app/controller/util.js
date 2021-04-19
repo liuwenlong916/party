0881ad164bc6bbaa07d0785e62eb67b78af6d8de
liuwenlong916 <1141657011@qq.com>
2021-04-19 10:59:42 +0800

    验证码

---
+++ server/app/controller/util.js
@@ -0,0 +1,17 @@
+const svgCaptcha = require("svg-captcha");
+const BaseController = require("./base");
+
+module.exports = class UtilController extends BaseController {
+  async captcha() {
+    const captcha = svgCaptcha.create({
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 40,
+      noise: 3,
+    });
+    this.ctx.session.captcha = captcha.text;
+    this.ctx.response.type = "image/svg+xml";
+    this.ctx.body = captcha.data;
+  }
+};
--- server/app/router.js
+++ server/app/router.js
@@ -6,6 +6,7 @@
 module.exports = (app) => {
   const { router, controller } = app;
   router.get("/", controller.home.index);
+  router.get("/captcha", controller.util.captcha);
   router.group({ name: "user", prefix: "/user" }, (router) => {
     const { info } = controller.user;
     router.get("/:user_id", info);
